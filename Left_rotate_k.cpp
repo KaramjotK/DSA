e328bbd611a9dbf0f0867ebf564aa11a9a0bba2c
KaramjotK <77890299+KaramjotK@users.noreply.github.com>
2025-12-31 03:00:16 +0530

    Refactor left_rotate array by k places
    Refactor left_rotate function to improve logic and efficiency.

--- Left_rotate_k.cpp
+++ Left_rotate_k.cpp
@@ -4,22 +4,41 @@ using namespace std;
 
 void left_rotate(vector<int> &arr, int k){
     int n = arr.size();
+    k = k % n;
     int temp[k];
     
     for (int i=0; i<k; i++){
         temp[i] = arr[i];
     }
-    int j = k;
-    for (j; j<n; j++){
+
+    for (int j=k; j<n; j++){
         arr[j-k] = arr[j];
     }
     
-    for(int i=0; i<k; i++){
-        arr[j-k] = temp[i];
-        j++;
+    for(int i=n-k; i<n; i++){
+        arr[i] = temp[i - (n-k)];
     }
     
 }
+
+// void left_rotate(vector<int> &arr, int k){
+//     int n = arr.size();
+//     int temp[k];
+    
+//     for (int i=0; i<k; i++){
+//         temp[i] = arr[i];
+//     }
+//     int j = k;
+//     for (j; j<n; j++){
+//         arr[j-k] = arr[j];
+//     }
+    
+//     for(int i=0; i<k; i++){
+//         arr[j-k] = temp[i];
+//         j++;
+//     }
+    
+// }
 
 int main() {
     vector<int> arr = {1,2,3,4,5,6,7};
