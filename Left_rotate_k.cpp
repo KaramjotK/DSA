99dcd569fb0aa6c1d0fe1c67b20e45a484d488d7
KaramjotK <77890299+KaramjotK@users.noreply.github.com>
2025-12-31 03:19:28 +0530

    Refactor left_rotate array by k places
    Implemented an optimal approach for left rotation using reversal.

--- Left_rotate_k.cpp
+++ Left_rotate_k.cpp
@@ -1,7 +1,7 @@
-// Online C++ compiler to run C++ program online
 #include <bits/stdc++.h>
 using namespace std;
 
+//BRUTE FORCE (extra space)
 //Learning - better index manipulation
 void left_rotate(vector<int> &arr, int k){
     int n = arr.size();
@@ -41,6 +41,28 @@ void left_rotate(vector<int> &arr, int k){
 //     }
     
 // }
+
+//OPTIMAL APPROACH
+//pass arr by referance, otherwise modifies copy
+void Reverse(vector<int> &arr, int start, int end){ 
+    while (start<=end){
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+    }
+}
+
+//pass arr by referance, otherwise modifies copy
+void left_rotate(vector<int> &arr, int k){
+    int n = arr.size();
+    k = k % n;
+    
+    Reverse(arr, 0, k-1);
+    Reverse(arr, k, n-1);
+    Reverse(arr, 0, n-1);
+}
 
 int main() {
     vector<int> arr = {1,2,3,4,5,6,7};
